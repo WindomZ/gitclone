d86e71b7a1621f5bb1ba3e7e138a3a8d973a5ec0
WindomZ <zwdsdtc@gmail.com>
2017-02-07 08:42:25 +0800

    add git clone dir impl

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,51 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"path"
+	"regexp"
+
 	"github.com/urfave/cli"
+	"github.com/whilp/git-urls"
 )
 
 var RootFlagAction = &FlagAction{
 	Flag:     nil,
 	FlagName: "",
-	Action: func(c *cli.Context, f *FlagAction) (bool, *cli.ExitError) {
+	Action: func(c *cli.Context, f *FlagAction) (bool, error) {
 		if len(c.Args()) == 0 {
-			return true, cli.NewExitError("No args!", 86) // error
+			// print usage
+			return true, errors.New("No args!") // error
 		}
-		println(fmt.Sprintf("root-value: %#v", c.Args())) // log
+		repo := c.Args().Get(0)
+		if !ValidGitAddress(repo) {
+			return true, errors.New(fmt.Sprintf("repository '%v' does not exist", repo))
+		}
+		u, err := giturls.Parse(repo)
+		if err != nil {
+			return true, err
+		}
+		f_dir := u.Host + u.Path
+		if ExistFile(DEFAULT_DIR_NAME) {
+			f_dir = path.Join(DEFAULT_DIR_NAME, f_dir)
+		}
+		//_, f_path := path.Split(f_dir)
+		//f_path = strings.Split(f_path, ".")[0]
+		if len(f_dir) != 0 /* && len(f_path) != 0*/ && !ExistFile(f_dir) {
+			if err := os.MkdirAll(f_dir, os.ModePerm); err != nil {
+				return true, err
+			}
+		}
+
 		return true, nil
 	},
+}
+
+func ValidGitAddress(repo string) bool {
+	matched, err := regexp.MatchString(
+		`((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?`,
+		repo)
+	return matched && err == nil
 }
