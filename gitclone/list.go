1e67e7b32a97370462495a17a2d2fd4f1228badc
WindomZ <git.windomz@gmail.com>
2017-03-22 13:49:38 +0800

    feat: command search implementation

--- gitclone/list.go
+++ gitclone/list.go
@@ -4,13 +4,15 @@ import "fmt"
 
 var ListAction = func() error {
 	dirs := getGitRepoDirList(getCurrentDirectory())
+
 	if len(dirs) == 0 {
 		fmt.Println("Not found any git repository!")
 		return nil
 	}
+
 	fmt.Println("Found the following git repositories:")
 	for i, dir := range dirs {
-		fmt.Printf("%d: %s\n", i, dir)
+		fmt.Printf("%4d >>> %s\n", i, dir)
 	}
 	return nil
 }
--- gitclone/search.go
+++ gitclone/search.go
@@ -1,8 +1,31 @@
 package gitclone
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/WindomZ/go-commander"
+	"strings"
+)
 
-var SearchAction = func() error {
-	fmt.Println("search...")
+var SearchAction = func(c commander.Context) error {
+	repo := strings.ToLower(c.GetString("<key>"))
+
+	dirs := getGitRepoDirList(getCurrentDirectory())
+
+	list := make([]string, 0, len(dirs))
+	for _, dir := range dirs {
+		if strings.Contains(strings.ToLower(dir), repo) {
+			list = append(list, dir)
+		}
+	}
+
+	if len(list) == 0 {
+		fmt.Println("Not found any matching git repository!")
+		return nil
+	}
+
+	fmt.Println("Found the following matching git repositories:")
+	for i, dir := range list {
+		fmt.Printf("%4d >>> %s\n", i, dir)
+	}
 	return nil
 }
